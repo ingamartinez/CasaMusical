d7ffe0ce26f623e357054663697fdd41afa83822
Alejandro Martinez <ing.amartinez94@gmail.com>
2016-12-27 03:38:10 -0500

    Css Search box

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,31 +57,35 @@
 
     <div class="header-middle"><!--header-middle-->
         <div class="container">
-            <div class="row">
+            <div class="row" style="display: flex">
                 <div class="col-sm-4">
                     <div class="logo pull-left">
                         {{--<a href="index.html"><img src="images/home/logo.png" alt=""/></a>--}}
                         <a href="index.html"><img class="img-resize-logo" src={{url('images/home/cm-logo.png')}} alt=""/></a>
                     </div>
                 </div>
-                <div class="col-sm-8">
-                    <div class="shop-menu pull-right">
-                        <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fa fa-user"></i> Cuenta</a></li>
-                            <li><a href="#"><i class="fa fa-star"></i> Lista de deseos</a></li>
-                            {{--<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>--}}
-                            {{--<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>--}}
-                            <li><a href="login.html"><i class="fa fa-lock"></i> Ingresar</a></li>
-                        </ul>
+                <div class="col-sm-8" style="display: flex;flex-direction: row;flex-wrap: wrap;">
+                    <div class="col-sm-12">
+                        <div class="shop-menu pull-right">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#"><i class="fa fa-user"></i> Cuenta</a></li>
+                                <li><a href="#"><i class="fa fa-star"></i> Lista de deseos</a></li>
+                                <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+                                <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+                                <li><a href="login.html"><i class="fa fa-lock"></i> Ingresar</a></li>
+
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-sm-8">
-                    <div class="shop-menu pull-right">
-                        <div class="search_box pull-right">
-                            <input type="text" placeholder="Buscar"/>
+                    <div class="col-sm-12" style="align-self: flex-end;">
+                        <div class="shop-menu pull-right">
+                            <div class="search_box pull-right">
+                                <input type="text" placeholder="Buscar"/>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div><!--/header-middle-->
